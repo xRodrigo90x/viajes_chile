00c048042be7c3a8cb53f3cf38ff6fa0f42c63f1
Rodrigo <rodrigo.donoso990@gmail.com>
2024-07-10 21:11:35 -0400

    se arregla dinamismo en el navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,27 @@
 
 
+let barraNavegacion = document.getElementById('nav');
 
 window.addEventListener('scroll', () => {
-    let barraNavegacion = document.getElementById('nav');
-
-
     let umbralScroll = 100;
-    let scroll = window.scrollY > umbralScroll
+    let scroll = window.scrollY > umbralScroll;
 
     if (scroll) {
-        barraNavegacion.classList.add("sticky")
-        barraNavegacion.classList.remove("container")
-
-        barraNavegacion.addEventListener("mouseover", () => {
-            barraNavegacion.classList.add("sticky2");
-            
-        });
-
+        barraNavegacion.classList.add("sticky");
+        barraNavegacion.classList.remove("container");
     } else {
-        barraNavegacion.classList.remove("sticky")
-        barraNavegacion.addEventListener("mouseout", () => {          
-
-                barraNavegacion.classList.remove("sticky2");
-            
-        });
-
-    };
-
+        barraNavegacion.classList.remove("sticky");
+        barraNavegacion.classList.add("container");
+    }
 });
 
+
+barraNavegacion.addEventListener("mouseover", () => {
+    if (barraNavegacion.classList.contains("sticky")) {
+        barraNavegacion.classList.add("sticky2");
+    }
+});
+
+barraNavegacion.addEventListener("mouseout", () => {
+    barraNavegacion.classList.remove("sticky2");
+});
